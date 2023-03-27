49b06c1a484256d8dd15ccabbeb4e95c4f6eb3f8
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-03-27 10:37:07 -0500

    Create propuesta.py

---
+++ pages/propuesta.py
@@ -0,0 +1,82 @@
+import streamlit as st
+
+from streamlit_echarts import st_echarts
+import pandas as pd
+import numpy as np
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from streamlit_echarts import st_pyecharts
+
+
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Solutions", page_icon="🧠")
+
+st.title('Novus Solutions 🧠 - Grupo IberoPistacho')
+st.header("CENTRALES DE MONITOREO DE MODELOS DE INTELIGENCIA ARTIFICIAL")
+
+
+st.write('Momento Óptimo de Cosecha')
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  acelerometro1 = {
+        "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+        "series": [
+            {
+                "name": "Pressure",
+                "type": "gauge",
+                "axisLine": {
+                    "lineStyle": {
+                        "width": 10,
+                    },
+                },
+                "progress": {"show": "true", "width": 10},
+                "detail": {"valueAnimation": "true", "formatter": "{value}"},
+                "data": [{"value": 90%, "name": "Cosecha"}],
+            }
+        ],
+    }
+
+  st_echarts(options=acelerometro1)
+
+with col2:
+  acelerometro2 = {
+        "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+        "series": [
+            {
+                "name": "Pressure",
+                "type": "gauge",
+                "axisLine": {
+                    "lineStyle": {
+                        "width": 10,
+                    },
+                },
+                "progress": {"show": "true", "width": 10},
+                "detail": {"valueAnimation": "true", "formatter": "{value}"},
+                "data": [{"value": 50, "name": "Producción"}],
+            }
+        ],
+    }
+
+  st_echarts(options=acelerometro2)
+
+with col3:
+  acelerometro3 = {
+        "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+        "series": [
+            {
+                "name": "Pressure",
+                "type": "gauge",
+                "axisLine": {
+                    "lineStyle": {
+                        "width": 10,
+                    },
+                },
+                "progress": {"show": "true", "width": 10},
+                "detail": {"valueAnimation": "true", "formatter": "{value}"},
+                "data": [{"value": 30%, "name": "Rentabilidad"}],
+            }
+        ],
+    }
+  st_echarts(options=acelerometro3)
